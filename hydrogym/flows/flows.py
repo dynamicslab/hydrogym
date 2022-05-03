f426369e5e5eef951135f3d8e686d23428cbc806
Jared Callaham <jared.callaham@gmail.com>
2022-05-03 10:09:44 -0400

    Added callbacks for I/O and restart

--- hydrogym/flows/flows.py
+++ hydrogym/flows/flows.py
@@ -15,7 +15,27 @@ class Flow:
         self.velocity_space = fd.VectorFunctionSpace(mesh, 'CG', 2)
         self.pressure_space = fd.FunctionSpace(mesh, 'CG', 1)
         self.mixed_space = fd.MixedFunctionSpace([self.velocity_space, self.pressure_space])
+        self.sol = fd.Function(self.mixed_space, name='q')
+        self.split_solution()  # Break out and rename solutio 
+
+    def save_checkpoint(self, h5_file):
+        with fd.CheckpointFile(h5_file, 'w') as chk:
+            chk.save_mesh(self.mesh)  # optional
+            chk.save_function(self.sol)
+
+    def load_checkpoint(self, h5_file):
+        with fd.CheckpointFile(h5_file, 'r') as chk:
+            mesh = chk.load_mesh('mesh')
+            Flow.__init__(self, mesh)  # Reinitialize with new mesh
+            self.sol = chk.load_function(self.mesh, 'q')
         
+        self.split_solution()  # Reset functions so self.u, self.p point to the new solution
+
+    def split_solution(self):
+        self.u, self.p = self.sol.split()
+        self.u.rename('u')
+        self.p.rename('p')
+
     def init_bcs(self, mixed=False):
         """Define all boundary conditions"""
         pass
@@ -52,13 +72,13 @@ class Flow:
 
     def solve_steady(self):
         self.init_bcs(mixed=True)
-        w = fd.Function(self.mixed_space)
+        q = self.sol
 
-        F = self.steady_form(w, fd.TestFunctions(self.mixed_space))  # Nonlinear variational form
-        J = fd.derivative(F, w)    # Jacobian
+        F = self.steady_form(q, fd.TestFunctions(self.mixed_space))  # Nonlinear variational form
+        J = fd.derivative(F, q)    # Jacobian
 
         bcs = self.collect_bcs()
-        problem = fd.NonlinearVariationalProblem(F, w, bcs, J)
+        problem = fd.NonlinearVariationalProblem(F, q, bcs, J)
         solver = fd.NonlinearVariationalSolver(problem)
         solver.solve()
 
@@ -66,11 +86,11 @@ class Flow:
 
 
 class Cylinder(Flow):
-    from .cylinder.mesh.common import INLET, FREESTREAM, OUTLET, CYLINDER
+    from .mesh.cylinder.mesh import INLET, FREESTREAM, OUTLET, CYLINDER
 
     def __init__(self, mesh_name='noack'):
-        mesh_root = f'{hydrogym.install_dir}/flows/cylinder/mesh'
-        mesh = fd.Mesh(f'{mesh_root}/{mesh_name}/cyl.msh')
+        from .mesh.cylinder.mesh import load_mesh
+        mesh = load_mesh(name=mesh_name)
 
         self.Re = fd.Constant(100)
         self.U_inf = fd.Constant((1.0, 0.0))
@@ -89,15 +109,15 @@ class Cylinder(Flow):
     def collect_bcp(self):
         return [self.bcp_outflow]
 
-    def steady_form(self, w, w_test):
-        (u, p) = fd.split(w)
-        (v, q) = w_test
+    def steady_form(self, q, q_test):
+        (u, p) = fd.split(q)
+        (v, s) = q_test
         nu = fd.Constant(1/self.Re)
 
         F  = dot(dot(u, nabla_grad(u)), v)*dx \
             + inner(self.sigma(u, p), self.epsilon(v))*dx \
             + dot(p*self.n, v)*ds - inner(nu*nabla_grad(u)*self.n, v)*ds \
-            + dot(div(u), q)*dx
+            + dot(div(u), s)*dx
         return F
 
     def compute_forces(self, u, p):
@@ -106,97 +126,3 @@ class Cylinder(Flow):
         CL = fd.assemble(2*force[1]*ds(self.CYLINDER))
         CD = fd.assemble(2*force[0]*ds(self.CYLINDER))
         return CL, CD
-
-    def integrate(self, dt=1e-3, Tf=0.01, output_dir=None):
-        num_steps = int(Tf//dt)
-        k = fd.Constant(dt)
-        nu = fd.Constant(1/self.Re)
-
-        self.init_bcs()
-        V, Q = self.function_spaces()
-
-        # Boundary conditions
-        bcu = self.collect_bcu()
-        bcp = self.collect_bcp()
-
-        u = fd.TrialFunction(V)
-        p = fd.TrialFunction(Q)
-        v = fd.TestFunction(V)
-        q = fd.TestFunction(Q)
-        u_n = fd.Function(V, name='u')
-        p_n = fd.Function(Q, name='p')
-        u_ = fd.Function(V)
-        p_ = fd.Function(Q)
-
-        U = 0.5*(u_n + u)  # Average for semi-implicit
-        u_t = (u - u_n)/k  # Time derivative
-
-        # Velocity predictor
-        F1 = dot(u_t, v)*dx \
-            + dot(dot(u_n, nabla_grad(u_n)), v)*dx \
-            + inner(self.sigma(U, p_n), self.epsilon(v))*dx \
-            + dot(p_n*self.n, v)*ds - dot(nu*nabla_grad(U)*self.n, v)*ds
-            # - dot(f, v)*self.dx
-        a1 = fd.lhs(F1)
-        L1 = fd.rhs(F1)
-
-        # Poisson equation
-        a2 = dot(nabla_grad(p), nabla_grad(q))*dx
-        L2 = dot(nabla_grad(p_n), nabla_grad(q))*dx - (1/k)*div(u_)*q*dx
-
-        # Projection step (pressure correction)
-        a3 = dot(u, v)*dx
-        L3 = dot(u_, v)*dx - k*dot(nabla_grad(p_ - p_n), v)*dx
-
-        # Assemble matrices
-        A1 = fd.assemble(a1, bcs=bcu)
-        A2 = fd.assemble(a2, bcs=bcp)
-        A3 = fd.assemble(a3)
-
-        # # Apply boundary conditions to matrices
-        # [bc.apply(A1) for bc in bcu]
-        # [bc.apply(A2) for bc in bcp]
-
-        # Create XDMF files for visualization output
-        if output_dir is not None:
-            pvd_out = fd.File(f"{output_dir}/solution.pvd")
-
-        t = 0
-        for n in range(num_steps):
-            t += dt  # Update current time
-
-            # Step 1: Tentative velocity step
-            b1 = fd.assemble(L1, bcs=bcu)
-            fd.solve(A1, u_.vector(), b1, solver_parameters={
-                "ksp_type": "gmres",
-                "pc_type": "hypre",
-                "pc_hypre_type": "boomeramg"
-            })
-
-            # Step 2: Pressure correction step
-            b2 = fd.assemble(L2, bcs=bcp)
-            fd.solve(A2, p_.vector(), b2, solver_parameters={
-                "ksp_type": "gmres",
-                "pc_type": "hypre",
-                "pc_hypre_type": "boomeramg"
-            })
-
-            # Step 3: Velocity correction step
-            b3 = fd.assemble(L3)
-            fd.solve(A3, u_.vector(), b3, solver_parameters={
-                "ksp_type": "cg",
-                "pc_type": "sor"
-            })
-
-            # Update previous solution
-            u_n.assign(u_)
-            p_n.assign(p_)
-
-            if output_dir is not None:
-                # Save solution to file (XDMF/HDF5)
-                pvd_out.write(u_n, p_n, time=t)
-                # xdmffile_u.write(u_, t)
-                # xdmffile_p.write(p_, t)
-
-            CL, CD = self.compute_forces(u_n, p_n)
-            print(f't:{t:08f}\t\t CL:{CL:08f} \t\tCD::{CD:08f}')
