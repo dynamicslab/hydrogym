77e2895f53788f5cb47827d7fef47506079504ab
Jared Callaham <jc244@uw.edu>
2022-08-16 14:48:02 -0700

    Modified cavity stability analysis for all unstable evals at Re=7500

--- examples/cavity/stability/run-stability.sh
+++ examples/cavity/stability/run-stability.sh
@@ -1,7 +1,7 @@
 mkdir global-modes
 
 mkdir tmp
-NPROC=24
+NPROC=64
 $VENV_COMPLEX/bin/mpiexec -np ${NPROC} $VENV_COMPLEX/bin/python stability.py
 $VENV/bin/mpiexec -np ${NPROC} $VENV/bin/python gather.py
 cp tmp/*.vtu tmp/*.pvtu tmp/*.pvd global-modes/
