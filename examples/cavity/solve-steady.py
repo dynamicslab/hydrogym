535c7a01113de153e0baa9707c57710edde9576c
Jared Callaham <jc244@uw.edu>
2022-08-18 05:28:39 -0700

    Formatting/linting cavity scripts

--- examples/cavity/solve-steady.py
+++ examples/cavity/solve-steady.py
@@ -1,23 +1,23 @@
 import firedrake as fd
-from firedrake.petsc import PETSc
-from ufl import curl
 
 import hydrogym as gym
 
-output_dir = 'output'
+output_dir = "output"
 Re = 2000
 Re_init = 1000
 
 # flow = gym.flow.Cavity(Re=Re, mesh='medium')
 
-h5_file = f'{output_dir}/{Re_init}_steady.h5'
+h5_file = f"{output_dir}/{Re_init}_steady.h5"
 flow = gym.flow.Cavity(Re=Re, h5_file=h5_file)
 
-solver_parameters={"snes_monitor": None,
-                   "ksp_type": "gmres",
-                   "mat_type": "aij",
-                   "pc_type": "lu",
-                   "pc_factor_mat_solver_type": "mumps"}
+solver_parameters = {
+    "snes_monitor": None,
+    "ksp_type": "gmres",
+    "mat_type": "aij",
+    "pc_type": "lu",
+    "pc_factor_mat_solver_type": "mumps",
+}
 
 # flow.set_control(1.0)
 flow.solve_steady(solver_parameters=solver_parameters)
--- examples/cavity/stability.py
+++ examples/cavity/stability.py
@@ -1,16 +1,17 @@
 import firedrake as fd
-from firedrake.petsc import PETSc
-from slepc4py import SLEPc
-from ufl import real
-
 import numpy as np
-assert PETSc.ScalarType == np.complex128, "Complex PETSc configuration required for stability analysis"
+from firedrake.petsc import PETSc
+from ufl import real
 
 import hydrogym as gym
 
-output_dir='output'
+assert (
+    PETSc.ScalarType == np.complex128
+), "Complex PETSc configuration required for stability analysis"
 
-## First we have to ramp up the Reynolds number to get the steady state
+output_dir = "output"
+
+# First we have to ramp up the Reynolds number to get the steady state
 Re_init = [500, 1000, 2000, 4000]
 flow = gym.flow.Cavity(Re=Re_init[0])
 gym.print(f"Steady solve at Re={Re_init[0]}")
@@ -22,44 +23,44 @@ for (i, Re) in enumerate(Re_init[1:]):
     qB = flow.solve_steady(solver_parameters={"snes_monitor": None})
 
 gym.print("Starting direct stability analysis")
-### First, direct analysis
+# First, direct analysis
 A, M = flow.linearize(qB)
 evals, es = gym.linalg.eig(A, M, num_eigenvalues=20, sigma=7.5j)
 max_idx = np.argmax(np.real(evals))
 nconv = es.getConverged()
 vr, vi = A.getVecs()
-gym.print(f'Re={Re}:\t\t{nconv} converged, largest: {evals[max_idx]}')
+gym.print(f"Re={Re}:\t\t{nconv} converged, largest: {evals[max_idx]}")
 
 es.getEigenpair(max_idx, vr, vi)
 evec_dir = fd.Function(flow.mixed_space)
 gym.utils.set_from_array(evec_dir, vr.array)
 
-pvd = fd.File('output/evec_dir.pvd')
+pvd = fd.File("output/evec_dir.pvd")
 u, p = evec_dir.split()
 vort = fd.project(fd.curl(u), flow.pressure_space)
-u.rename('u')
-p.rename('p')
-vort.rename('vort')
+u.rename("u")
+p.rename("p")
+vort.rename("vort")
 pvd.write(u, p, vort)
 
-np.save('output/evals.npy', evals)
+np.save("output/evals.npy", evals)
 
-### Adjoint
+# Adjoint
 A, M = flow.linearize(qB, adjoint=True)
 evals, es = gym.linalg.eig(A, M, num_eigenvalues=20, sigma=7.5j)
 max_idx = np.argmax(np.real(evals))
 nconv = es.getConverged()
 vr, vi = A.getVecs()
-gym.print(f'Re={Re}:\t\t{nconv} converged, largest: {evals[max_idx]}')
+gym.print(f"Re={Re}:\t\t{nconv} converged, largest: {evals[max_idx]}")
 
 es.getEigenpair(max_idx, vr, vi)
 evec_adj = fd.Function(flow.mixed_space)
 gym.utils.set_from_array(evec_adj, vr.array)
 
-pvd = fd.File('output/evec_adj.pvd')
+pvd = fd.File("output/evec_adj.pvd")
 u, p = evec_adj.split()
 vort = fd.project(fd.curl(u), flow.pressure_space)
-u.rename('u')
-p.rename('p')
-vort.rename('vort')
+u.rename("u")
+p.rename("p")
+vort.rename("vort")
 pvd.write(u, p, vort)
