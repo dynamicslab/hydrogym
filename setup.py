b59190e71cca8e5a6d0c8f8fa2f6957a458c4735
Ludger Paehler <ludger.paehler@tum.de>
2022-12-14 03:36:27 +0100

    Check python version.

--- setup.py
+++ setup.py
@@ -18,3 +18,27 @@ except ImportError:
     except ImportError:
         pass
 
+
+min_python_version = "3.8"
+max_python_version = "3.12"
+
+
+def _guard_py_ver():
+    if _version_module is None:
+        return
+
+    parse = _version_module.parse
+
+    min_py = parse(min_python_version)
+    max_py = parse(max_python_version)
+    cur_py = parse('.'.join(map(str, sys.version_info[:3])))
+
+    if not min_py <= cur_py < max_py:
+        msg = ('Cannot install on Python version {}; only versions >={}, <{} '
+                'are supported.')
+        raise RuntimeError(msg.format(cur_py, min_py, max_py))
+
+
+_guard_py_ver()
+
+
