5f7fbbb3784ba5dff8280ebc436932b55d074463
Ludger Paehler <ludger.paehler@tum.de>
2022-12-14 15:02:42 +0100

    Write new setup file which internalizes all the dependencies.

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ from distutils.command.build_ext import build_ext
 from distutils.spawn import spawn
 
 from setuptools import Extension, find_packages, setup
-import versioneer
 
 _version_module = None
 try:
@@ -42,3 +41,80 @@ def _guard_py_ver():
 _guard_py_ver()
 
 
+def get_ext_modules():
+    """
+    Return a list of Extension instances for the setup() call.
+    """
+
+    if os.getenv("HYDROGYM_DISABLE_FIREDRAKE"):
+        print("Firedrake engine disabled")
+    else:
+        # Search for Firedrake to see if it is already available
+        firedrake_root = os.getenv("FIREDRAKEROOT")
+
+        if firedrake_root:
+            print("Using Firedrake from:", firedrake_root)
+            # TODO(ludgerpaehler): This path here needs to filled with content
+            #      and to actually be tested by using a Firedrake from a
+            #      different virtualenv.
+        else:
+            print("Firedrake not found")
+
+    ext_modules = []
+
+    return ext_modules
+
+build_requires = []
+install_requires = [
+    'jupyterlab',
+    'ipykernel',
+    'notebook',
+    'ipywidgets',
+    'mpltools',
+    'nbformat',
+    'nbconvert',
+    'dmsuite',
+    'gym',
+    'memory_profiler',
+    'gmsh',
+    'modred',
+    'seaborn',
+    'control',
+    '--no-binary=h5py h5py',
+    'ray[rllib]',
+    'torch',
+    'evotorch',
+    'tensorboard',
+]
+
+metadata = dict(
+    name="hydrogym",
+    description="A Reinforcement Learning Environment for Fluid Dynamics",
+    version="0.1",
+    classifiers=[
+          "Development Status :: 2 - Pre-Alpha",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: MacOS",
+          "Operating System :: Unix",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Programming Language :: Python :: 3.10",
+          "Programming Language :: Python :: 3.11",
+          "Topic :: Scientific/Engineering :: Artificial Intelligence",
+          "Topic :: Scientific/Engineering :: Atmospheric Science",
+          "Topic :: Scientific/Engineering :: Hydrology",
+          "Topic :: Scientific/Engineering :: Physics",
+    ],
+    setup_requires=build_requires,
+    install_requires=install_requires,
+    python_requires=">={}".format(min_python_version),
+    license="MIT",
+)
+
+with open('README.md') as f:
+    metadata["long_description"] = f.read()
+
+setup(**metadata)
