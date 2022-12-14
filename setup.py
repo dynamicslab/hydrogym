590e7e4a52c11805ae320e1549c5c7995c63e202
Ludger Paehler <ludger.paehler@tum.de>
2022-12-14 03:28:49 +0100

    Add initial logic.

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from setuptools import setup
+import os
+import platform
+import sys
+from distutils import sysconfig
+from distutils.command import build
+from distutils.command.build_ext import build_ext
+from distutils.spawn import spawn
 
-setup(
-    name="hydrogym",
-    version="0.1.0",
-    # packages=['jupyter',
-    #           'jupyterhub',
-    #           'ipykernel',
-    #           'notebook',
-    #           'ipywidgets',
-    #           'mpltools',
-    #           'nbformat',
-    #           'nbconvert',
-    #           'dmsuite',
-    #           'gym',
-    #           'memory_profiler',
-    #           'gmsh']
-    packages=[],
-)
+from setuptools import Extension, find_packages, setup
+import versioneer
+
+_version_module = None
+try:
+    from packaging import version as _version_module
+except ImportError:
+    try:
+        from setuptools._vendor.packaging import version as _version_module
+    except ImportError:
+        pass
+
