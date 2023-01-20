f67a7e97c625b59b629941f91bdb61efc986dfa9
Jared Callaham <jared.callaham@gmail.com>
2023-01-20 07:55:17 -0500

    Testing unsteady cavity sim (coarse mesh)

--- examples/cavity/unsteady.py
+++ examples/cavity/unsteady.py
@@ -1,4 +1,5 @@
 import firedrake as fd
+import psutil
 
 import hydrogym.firedrake as hgym
 
@@ -32,20 +33,23 @@ def log_postprocess(flow):
     KE = 0.5 * fd.assemble(fd.inner(flow.u, flow.u) * fd.dx)
     TKE = flow.evaluate_objective()
     CFL = flow.max_cfl(dt)
-    return [CFL, KE, TKE]
+    mem_usage = psutil.virtual_memory().percent
+    return [CFL, KE, TKE, mem_usage]
 
 
 def compute_vort(flow):
     return (flow.u, flow.p, flow.vorticity())
 
 
-print_fmt = "t: {0:0.3f}\t\tCFL: {1:0.3f}\t\t KE: {2:0.12e}\t\t TKE: {3:0.12e}"
+print_fmt = (
+    "t: {0:0.3f}\t\tCFL: {1:0.3f}\t\t KE: {2:0.6e}\t\t TKE: {3:0.6e}\t\t Mem: {4:0.1f}"
+)
 callbacks = [
     hgym.io.ParaviewCallback(interval=100, filename=pvd_out, postprocess=compute_vort),
     hgym.io.CheckpointCallback(interval=100, filename=checkpoint),
     hgym.io.LogCallback(
         postprocess=log_postprocess,
-        nvals=3,
+        nvals=4,
         interval=10,
         filename=f"{output_dir}/stats.dat",
         print_fmt=print_fmt,
