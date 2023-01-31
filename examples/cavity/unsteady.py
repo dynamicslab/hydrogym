ca6d2ee397bdce157a5f128e52203e24636c8a38
Jared Callaham <jared.callaham@gmail.com>
2023-01-31 09:07:08 -0500

    Working cavity transient sim

--- examples/cavity/unsteady.py
+++ examples/cavity/unsteady.py
@@ -32,7 +32,8 @@ dt = 2.5e-4
 def log_postprocess(flow):
     KE = 0.5 * fd.assemble(fd.inner(flow.u, flow.u) * fd.dx)
     TKE = flow.evaluate_objective()
-    CFL = flow.max_cfl(dt)
+    # CFL = flow.max_cfl(dt)
+    CFL = 0.0
     mem_usage = psutil.virtual_memory().percent
     return [CFL, KE, TKE, mem_usage]
 
@@ -45,13 +46,13 @@ print_fmt = (
     "t: {0:0.3f}\t\tCFL: {1:0.3f}\t\t KE: {2:0.6e}\t\t TKE: {3:0.6e}\t\t Mem: {4:0.1f}"
 )
 callbacks = [
-    hgym.io.ParaviewCallback(interval=100, filename=pvd_out, postprocess=compute_vort),
-    hgym.io.CheckpointCallback(interval=100, filename=checkpoint),
+    # hgym.io.ParaviewCallback(interval=100, filename=pvd_out, postprocess=compute_vort),
+    # hgym.io.CheckpointCallback(interval=1000, filename=checkpoint),
     hgym.io.LogCallback(
         postprocess=log_postprocess,
         nvals=4,
         interval=10,
-        filename=f"{output_dir}/stats.dat",
+        # filename=f"{output_dir}/stats.dat",
         print_fmt=print_fmt,
     ),
 ]
--- examples/demo/run-transient.py
+++ examples/demo/run-transient.py
@@ -1,3 +1,5 @@
+import psutil
+
 import hydrogym.firedrake as hgym
 
 output_dir = "."
@@ -13,18 +15,25 @@ dt = 1e-2
 
 
 def log_postprocess(flow):
-    return flow.get_observations()
+    mem_usage = psutil.virtual_memory().percent
+    CL, CD = flow.get_observations()
+    return CL, CD, mem_usage
 
 
 # Set up the callback
-print_fmt = (
-    "t: {0:0.2f},\t\t CL: {1:0.3f},\t\t CD: {2:0.03f}"  # This will format the output
-)
+print_fmt = "t: {0:0.2f},\t\t CL: {1:0.3f},\t\t CD: {2:0.03f}\t\t Mem: {3:0.1f}"  # This will format the output
 log = hgym.utils.io.LogCallback(
-    postprocess=log_postprocess, nvals=2, interval=1, print_fmt=print_fmt, filename=None
+    postprocess=log_postprocess,
+    nvals=3,
+    interval=10,
+    print_fmt=print_fmt,
+    filename=None,
 )
 
-callbacks = [log, hgym.utils.io.CheckpointCallback(interval=100, filename=checkpoint)]
+# callbacks = [log, hgym.utils.io.CheckpointCallback(interval=100, filename=checkpoint)]
+callbacks = [
+    log,
+]
 
 hgym.print("Beginning integration")
 hgym.integrate(flow, t_span=(0, Tf), dt=dt, callbacks=callbacks, method="IPCS")
