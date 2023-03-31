bdb3ef40d55cd977bf91c124e8de031899b3e5a3
Jared Callaham <jared.callaham@gmail.com>
2023-03-31 11:14:37 -0400

    Remove differentiability (#65)
    * Removed differentiability, tests passing
    
    * Fixed pre-commit issues
    
    * Fixed pre-commit issues in tests

--- hydrogym/firedrake/__init__.py
+++ hydrogym/firedrake/__init__.py
@@ -2,7 +2,7 @@ from hydrogym.core import FlowEnv
 
 from .actuator import DampedActuator
 from .flow import FlowConfig
-from .solver import IPCS, IPCS_diff, NewtonSolver, integrate
+from .solver import IPCS, NewtonSolver, integrate
 from .utils import io, is_rank_zero, linalg, modeling, print
 
 from .envs import Cylinder, Pinball, Cavity, Step  # isort:skip
