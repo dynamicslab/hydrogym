1c2625d10f8152e3d8cc67dc013de335618ecdd7
root <sammahnert@gmail.com>
2023-11-06 15:01:54 -0800

    Using the l2 (discrete) Riesz Representation to obtain control as a Function in cooperation with the new CoFunction paradigm of Firedrake

--- hydrogym/firedrake/flow.py
+++ hydrogym/firedrake/flow.py
@@ -237,7 +237,7 @@ class FlowConfig(PDEBase):
 
             # Control as Function
             B.append(
-                fd.assemble(inner(fd.Constant((0, 0)), v) * dx, bcs=self.collect_bcs())
+                fd.assemble(inner(fd.Constant((0, 0)), v) * dx, bcs=self.collect_bcs()).riesz_representation(riesz_map = 'l2')
             )
 
             # Have to have mixed function space for computing B functions
