585a7dd59d48a04693093917c56e9765f5f0b3be
Jared Callaham <jc244@uw.edu>
2024-03-02 12:04:19 +0000

    Jet control for cylinder

--- hydrogym/firedrake/flow.py
+++ hydrogym/firedrake/flow.py
@@ -233,7 +233,10 @@ class FlowConfig(PDEBase):
 
             if hasattr(self, "bcu_actuation"):
                 for i in range(self.ACT_DIM):
-                    self.bcu_actuation[i].set_scale(self.actuators[i].state)
+                    u = np.clip(
+                        self.actuators[i].state, -self.MAX_CONTROL, self.MAX_CONTROL
+                    )
+                    self.bcu_actuation[i].set_scale(u)
 
     def control_vec(self, mixed=False):
         """Functions corresponding to the columns of the control matrix"""
