e37060b96f146f8fc10a95d6e10ad4f61be948c8
Jared Callaham <jc244@uw.edu>
2024-03-11 10:07:50 +0000

    Remove IPCS solver

--- hydrogym/firedrake/solvers/__init__.py
+++ hydrogym/firedrake/solvers/__init__.py
@@ -1,11 +1,9 @@
 from .base import NewtonSolver
 from .bdf_ext import SemiImplicitBDF
 from .integrate import integrate
-from .ipcs import IPCS
 
 __all__ = [
     "NewtonSolver",
-    "IPCS",
     "SemiImplicitBDF",
     "integrate",
 ]
