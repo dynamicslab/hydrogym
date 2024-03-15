05583db6aa0b9549983029d30320b2661b048e0d
Ludger Paehler <ludger.paehler@tum.de>
2024-03-15 12:20:18 +0000

    Move gradient tests out to dedicated test files.

---
+++ test/test_step_grad.py
@@ -0,0 +1,21 @@
+import firedrake_adjoint as fda
+from ufl import sin
+
+import hydrogym.firedrake as hgym
+
+
+def test_grad():
+    flow = hgym.Step(Re=100, mesh="coarse")
+
+    c = fda.AdjFloat(0.0)
+    flow.set_control(c)
+
+    solver = hgym.NewtonSolver(flow)
+    solver.solve()
+
+    (y,) = flow.get_observations()
+
+    dy = fda.compute_gradient(y, fda.Control(c))
+
+    print(dy)
+    assert abs(dy) > 0
