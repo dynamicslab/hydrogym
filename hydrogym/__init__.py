9df8e649f8b151a0617694d1ed4bb1d1f96f7859
Ludger Paehler <ludger.paehler@tum.de>
2024-10-27 16:59:18 +0100

    Clean up to remove experimental features from the core

--- hydrogym/__init__.py
+++ hydrogym/__init__.py
@@ -1,5 +1,2 @@
-from . import distributed, firedrake, torch_env
+from . import distributed, firedrake
 from .core import CallbackBase, FlowEnv, PDEBase, TransientSolver
-from .core_1DEnvs import OneDimEnv, PDESolverBase1D
-
-from .torch_env import Kuramoto_Sivashinsky, Burgers # isort:skip
