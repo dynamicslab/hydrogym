7da18ab2acf7397eae7c90e2da1e65ef4522fad0
Ludger Paehler <ludger.paehler@tum.de>
2025-03-20 04:07:25 -0700

    Yapf format.

--- reproducer_rewrite_ppo_train.py
+++ reproducer_rewrite_ppo_train.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ["OMP_NUM_THREADS"] = "1"
 
 from ray.rllib.algorithms.ppo import PPOConfig
@@ -9,25 +10,22 @@ from tap import Tap
 
 import hydrogym
 
-
 logging.set_log_level(logging.DEBUG)
 
 
 class ArgumentParser(Tap):
-    reynolds_number: int = 100  # Reynolds number
-    mesh_resolution: str = "coarse"  # Mesh resolution
-    time_step: float = 1e-2  # Time step
-    learning_rate: float = 0.0002  # Learning rate
-    batch_size_per_learner: int = 2000  # Batch size per learner
-    number_of_epochs: int = 10  # Number of epochs
+  reynolds_number: int = 100  # Reynolds number
+  mesh_resolution: str = "coarse"  # Mesh resolution
+  time_step: float = 1e-2  # Time step
+  learning_rate: float = 0.0002  # Learning rate
+  batch_size_per_learner: int = 2000  # Batch size per learner
+  number_of_epochs: int = 10  # Number of epochs
 
 
 # Read in the command-line arguments
 args = ArgumentParser().parse_args()
 
-
 # TODO: Might want to consider monkey-patching the environment in at this point for easier testing.
-
 
 # Define the logging callback
 log = hydrogym.firedrake.utils.io.LogCallback(
@@ -40,8 +38,7 @@ log = hydrogym.firedrake.utils.io.LogCallback(
 
 # Create a config instance for the PPO algorithm.
 config = (
-    PPOConfig()
-    .environment(
+    PPOConfig().environment(
         "hydrogym.FlowEnv",
         env_config={
             "flow": hydrogym.firedrake.Cylinder,
@@ -56,20 +53,19 @@ config = (
             #"callbacks": [log],
             "max_steps": 10000,  # --> This should be going into the `training`
         },
-    )
-    .env_runners(num_env_runners=2)
-    .training(
-        lr = args.learning_rate,
-        train_batch_size_per_learner = args.batch_size_per_learner,
-        num_epochs = args.number_of_epochs,
-    )
-)
+    ).env_runners(num_env_runners=2).training(
+        lr=args.learning_rate,
+        train_batch_size_per_learner=args.batch_size_per_learner,
+        num_epochs=args.number_of_epochs,
+    ))
 
 # Construct the actual algorithm
 ppo = config.build_algo()
 
 for _ in range(4):
-    pprint(ppo.train())
+  pprint(ppo.train())
 
 # Store the trained algorithm
-checkpoint_path = ppo.save_to_path("/Users/lpaehler/Work/ReinforcementLearning/hydrogym-issue-fix/ppo_checkpoint")
+checkpoint_path = ppo.save_to_path(
+    "/Users/lpaehler/Work/ReinforcementLearning/hydrogym-issue-fix/ppo_checkpoint"
+)
--- reproducer_rewrite_ppo_tune.py
+++ reproducer_rewrite_ppo_tune.py
@@ -2,12 +2,8 @@ from ray import train, tune
 from ray.rllib.algorithms.ppo import PPOConfig
 
 config = (
-    PPOConfig()
-    .environment("Pendulum-v1")
-    .training(
-        lr=tune.grid_search([0.001, 0.0005, 0.0001]),
-    )
-)
+    PPOConfig().environment("Pendulum-v1").training(
+        lr=tune.grid_search([0.001, 0.0005, 0.0001]),))
 
 # Creating a tuner instance to manage the trials
 tuner = tune.Tuner(
@@ -18,8 +14,7 @@ tuner = tune.Tuner(
     # ``.train()``. Also note that -1100 is not a good episode return for
     # Pendulum-v1, we are using it here to shorten the experiment time.
     run_config=train.RunConfig(
-        stop={"env_runners/episode_return_mean": -1100.0},
-    ),
+        stop={"env_runners/episode_return_mean": -1100.0},),
 )
 # Run the Tuner and capture the results.
 results = tuner.fit()
