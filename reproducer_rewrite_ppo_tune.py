361562ddc820acc97b62af60685cebf40837c36a
Ludger Paehler <ludger.paehler@tum.de>
2025-03-27 06:05:14 +0000

    Add example configuration for Ray Tune.

--- reproducer_rewrite_ppo_tune.py
+++ reproducer_rewrite_ppo_tune.py
@@ -1,11 +1,150 @@
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"
+
 from ray import train, tune
 from ray.rllib.algorithms.ppo import PPOConfig
 
+from firedrake import logging
+import numpy as np
+from tap import Tap
+from typing import Iterable, Tuple, Union
+from numpy.typing import ArrayLike
+import gymnasium as gym
+
+import hydrogym
+from hydrogym.core import PDEBase, TransientSolver, CallbackBase
+
+logging.set_log_level(logging.DEBUG)
+
+class ArgumentParser(Tap):
+    reynolds_number: int = 100  # Reynolds number
+    mesh_resolution: str = "medium"  # Mesh resolution
+    time_step: float = 1e-2  # Time step
+    batch_size_per_learner: int = 2000  # Batch size per learner
+    number_of_epochs: int = 10  # Number of epochs
+    num_runners: int = 16  # Number of environment runners
+
+# Read in the command-line arguments
+args = ArgumentParser().parse_args()
+
+class FlowEnv(gym.Env):
+    def __init__(self, env_config: dict):
+        self.flow: PDEBase = env_config.get("flow")(**env_config.get("flow_config", {}))
+        self.solver: TransientSolver = env_config.get("solver")(self.flow, **env_config.get("solver_config", {}))
+        self.callbacks: Iterable[CallbackBase] = env_config.get("callbacks", [])
+        self.max_steps: int = env_config.get("max_steps", int(1e6))
+        self.iter: int = 0
+        self.q0: self.flow.StateType = self.flow.copy_state()
+
+        self.observation_space = gym.spaces.Box(
+            low=-np.inf,
+            high=np.inf,
+            shape=(self.flow.num_outputs,),
+            dtype=float,
+        )
+        self.action_space = gym.spaces.Box(
+            low=-self.flow.MAX_CONTROL,
+            high=self.flow.MAX_CONTROL,
+            shape=(self.flow.num_inputs,),
+            dtype=float,
+        )
+
+    def set_callbacks(self, callbacks: Iterable[CallbackBase]):
+        self.callbacks = callbacks
+
+    def step(self, action: Iterable[ArrayLike] = None) -> Tuple[ArrayLike, float, bool, dict]:
+        """Advance the state of the environment.  See gymnasium.Env documentation
+
+        Args:
+            action (Iterable[ArrayLike], optional): Control inputs. Defaults to None.
+
+        Returns:
+            Tuple[ArrayLike, float, bool, dict]: obs, reward, done, info
+        """
+        self.solver.step(self.iter, control=action)
+        self.iter += 1
+        t = self.iter * self.solver.dt
+        for cb in self.callbacks:
+            cb(self.iter, t, self.flow)
+        obs = self.flow.get_observations()
+
+        reward = self.get_reward()
+        done = self.check_complete()
+        info = {}
+
+        # Truncation=False as we handle it through the max_steps for now
+        truncated = False
+
+        obs = self.stack_observations(obs)
+
+        return obs, reward, done, truncated, info
+
+    def stack_observations(self, obs):
+        return obs
+
+    def get_reward(self):
+        return -self.solver.dt * self.flow.evaluate_objective()
+
+    def check_complete(self):
+        return self.iter > self.max_steps
+
+    def reset(self, seed=None, t=0.0, options=None) -> Union[ArrayLike, Tuple[ArrayLike, dict]]:
+        super().reset(seed=seed)
+        self.iter = 0
+        self.flow.reset(q0=self.q0, t=t)
+        self.solver.reset()
+
+        info = {}
+
+        return self.flow.get_observations(), info
+
+    def render(self, mode="human", **kwargs):
+        self.flow.render(mode=mode, **kwargs)
+
+    def close(self):
+        for cb in self.callbacks:
+            cb.close()
+
+
+# Define the logging callback
+log = hydrogym.firedrake.utils.io.LogCallback(
+    postprocess=lambda flow: flow.get_observations(),
+    nvals=2,
+    interval=1,
+    print_fmt="t: {0:0.2f},\t\t CL: {1:0.3f},\t\t CD: {2:0.03f}",
+    filename=None,
+)
+
+# Dictionary of the flow configuration
+flow_dict = {
+    "flow": hydrogym.firedrake.Cylinder,
+    "flow_config": {
+        "Re": args.reynolds_number,
+        "mesh": args.mesh_resolution,
+    },
+    "solver": hydrogym.firedrake.SemiImplicitBDF,
+    "solver_config": {
+        "dt": args.time_step,
+    },
+    "callbacks": [log],
+    "max_steps": 10000,
+}
+
 config = (
     PPOConfig()
-    .environment("Pendulum-v1")
+    .environment(
+        FlowEnv,
+        env_config=flow_dict,
+    )
+    .env_runners(
+        num_env_runners=args.num_runners,
+        sample_timeout_s = 300.0
+    )
     .training(
-        lr=tune.grid_search([0.001, 0.0005, 0.0001]),
+        lr=tune.grid_search([0.0005, 0.0002, 0.0001]),
+        train_batch_size_per_learner=args.batch_size_per_learner,
+        num_epochs=args.number_of_epochs,
     )
 )
 
@@ -15,10 +154,9 @@ tuner = tune.Tuner(
     param_space=config,
     # Specify a stopping criterion. Note that the criterion has to match one of the
     # pretty printed result metrics from the results returned previously by
-    # ``.train()``. Also note that -1100 is not a good episode return for
-    # Pendulum-v1, we are using it here to shorten the experiment time.
+    # ``.train()``.
     run_config=train.RunConfig(
-        stop={"env_runners/episode_return_mean": -1100.0},
+        stop={"CD": 0.2},
     ),
 )
 # Run the Tuner and capture the results.
