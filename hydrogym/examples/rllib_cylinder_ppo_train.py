686444df025e7dbbf20ff2ffd89358b58328567f
Ludger Paehler <ludger.paehler@tum.de>
2025-03-31 10:49:17 +0000

    Clean up examples

--- hydrogym/examples/rllib_cylinder_ppo_train.py
+++ hydrogym/examples/rllib_cylinder_ppo_train.py
@@ -6,9 +6,6 @@ from tap import Tap
 
 import hydrogym
 
-os.environ["OMP_NUM_THREADS"] = "1"
-logging.set_log_level(logging.DEBUG)
-
 
 class ArgumentParser(Tap):
     reynolds_number: int = 100  # Reynolds number
@@ -23,54 +20,64 @@ class ArgumentParser(Tap):
     )
 
 
-# Read in the command-line arguments
-args = ArgumentParser().parse_args()
+def main():
+    # Read in the command-line arguments
+    args = ArgumentParser().parse_args()
 
-# Define the logging callback
-log = hydrogym.firedrake.utils.io.LogCallback(
-    postprocess=lambda flow: flow.get_observations(),
-    nvals=2,
-    interval=1,
-    print_fmt="t: {0:0.2f},\t\t CL: {1:0.3f},\t\t CD: {2:0.03f}",
-    filename=None,
-)
+    # Set the number of OpenMP threads to 1 to improve the performance of Firedrake
+    os.environ["OMP_NUM_THREADS"] = "1"
 
+    # Return Debug logging info
+    logging.set_log_level(logging.DEBUG)
 
-# Dictionary of the flow configuration
-flow_dict = {
-    "flow": hydrogym.firedrake.Cylinder,
-    "flow_config": {
-        "Re": args.reynolds_number,
-        "mesh": args.mesh_resolution,
-    },
-    "solver": hydrogym.firedrake.SemiImplicitBDF,
-    "solver_config": {
-        "dt": args.time_step,
-    },
-    "callbacks": [log],
-    "max_steps": 10000,
-}
-
-# Create a config instance for the PPO algorithm.
-config = (
-    PPOConfig()
-    .environment(
-        hydrogym.FlowEnv,
-        env_config=flow_dict,
+    # Define the logging callback
+    log = hydrogym.firedrake.utils.io.LogCallback(
+        postprocess=lambda flow: flow.get_observations(),
+        nvals=2,
+        interval=1,
+        print_fmt="t: {0:0.2f},\t\t CL: {1:0.3f},\t\t CD: {2:0.03f}",
+        filename=None,
     )
-    .env_runners(num_env_runners=args.num_runners, sample_timeout_s=300.0)
-    .training(
-        lr=args.learning_rate,
-        train_batch_size_per_learner=args.batch_size_per_learner,
-        num_epochs=args.number_of_epochs,
+
+    # Dictionary of the flow configuration
+    flow_dict = {
+        "flow": hydrogym.firedrake.Cylinder,
+        "flow_config": {
+            "Re": args.reynolds_number,
+            "mesh": args.mesh_resolution,
+        },
+        "solver": hydrogym.firedrake.SemiImplicitBDF,
+        "solver_config": {
+            "dt": args.time_step,
+        },
+        "callbacks": [log],
+        "max_steps": 10000,
+    }
+
+    # Create a config instance for the PPO algorithm.
+    config = (
+        PPOConfig()
+        .environment(
+            hydrogym.FlowEnv,
+            env_config=flow_dict,
+        )
+        .env_runners(num_env_runners=args.num_runners, sample_timeout_s=300.0)
+        .training(
+            lr=args.learning_rate,
+            train_batch_size_per_learner=args.batch_size_per_learner,
+            num_epochs=args.number_of_epochs,
+        )
     )
-)
 
-# Construct the actual algorithm
-ppo = config.build_algo()
+    # Construct the actual algorithm
+    ppo = config.build_algo()
 
-for _ in range(4):
-    pprint(ppo.train())
+    for _ in range(4):
+        pprint(ppo.train())
 
-# Store the trained algorithm
-checkpoint_path = ppo.save_to_path(args.checkpoint_path)
+    # Store the trained algorithm
+    ppo.save_to_path(args.checkpoint_path)
+
+
+if __name__ == "__main__":
+    main()
--- hydrogym/examples/rllib_cylinder_ppo_tune.py
+++ hydrogym/examples/rllib_cylinder_ppo_tune.py
@@ -6,9 +6,6 @@ from tap import Tap
 
 import hydrogym
 
-os.environ["OMP_NUM_THREADS"] = "1"
-logging.set_log_level(logging.DEBUG)
-
 
 class ArgumentParser(Tap):
     reynolds_number: int = 100  # Reynolds number
@@ -19,59 +16,70 @@ class ArgumentParser(Tap):
     num_runners: int = 8  # Number of environment runners
 
 
-# Read in the command-line arguments
-args = ArgumentParser().parse_args()
+def main():
+    # Read in the command-line arguments
+    args = ArgumentParser().parse_args()
 
+    # Set the number of OpenMP threads to 1 to improve the performance of Firedrake
+    os.environ["OMP_NUM_THREADS"] = "1"
 
-# Define the logging callback
-log = hydrogym.firedrake.utils.io.LogCallback(
-    postprocess=lambda flow: flow.get_observations(),
-    nvals=2,
-    interval=1,
-    print_fmt="t: {0:0.2f},\t\t CL: {1:0.3f},\t\t CD: {2:0.03f}",
-    filename=None,
-)
+    # Return Debug logging info
+    logging.set_log_level(logging.DEBUG)
 
-# Dictionary of the flow configuration
-flow_dict = {
-    "flow": hydrogym.firedrake.Cylinder,
-    "flow_config": {
-        "Re": args.reynolds_number,
-        "mesh": args.mesh_resolution,
-    },
-    "solver": hydrogym.firedrake.SemiImplicitBDF,
-    "solver_config": {
-        "dt": args.time_step,
-    },
-    "callbacks": [log],
-    "max_steps": 10000,
-}
-
-config = (
-    PPOConfig()
-    .environment(
-        hydrogym.FlowEnv,
-        env_config=flow_dict,
+    # Define the logging callback
+    log = hydrogym.firedrake.utils.io.LogCallback(
+        postprocess=lambda flow: flow.get_observations(),
+        nvals=2,
+        interval=1,
+        print_fmt="t: {0:0.2f},\t\t CL: {1:0.3f},\t\t CD: {2:0.03f}",
+        filename=None,
     )
-    .env_runners(num_env_runners=args.num_runners, sample_timeout_s=300.0)
-    .training(
-        lr=tune.grid_search([0.0005, 0.0002, 0.0001]),
-        train_batch_size_per_learner=args.batch_size_per_learner,
-        num_epochs=args.number_of_epochs,
-    )
-)
 
-# Creating a tuner instance to manage the trials
-tuner = tune.Tuner(
-    config.algo_class,
-    param_space=config,
-    # Specify a stopping criterion. Note that the criterion has to match one of the
-    # pretty printed result metrics from the results returned previously by
-    # ``.train()``
-    # TODO: Doesn't work properly so far. The stop condition is not read in.
-    run_config=train.RunConfig(
-        stop={"CD": 0.2},
-    ),
-)
-# Run the Tuner and capture the results.
-results = tuner.fit()
+    # Dictionary of the flow configuration
+    flow_dict = {
+        "flow": hydrogym.firedrake.Cylinder,
+        "flow_config": {
+            "Re": args.reynolds_number,
+            "mesh": args.mesh_resolution,
+        },
+        "solver": hydrogym.firedrake.SemiImplicitBDF,
+        "solver_config": {
+            "dt": args.time_step,
+        },
+        "callbacks": [log],
+        "max_steps": 10000,
+    }
+
+    config = (
+        PPOConfig()
+        .environment(
+            hydrogym.FlowEnv,
+            env_config=flow_dict,
+        )
+        .env_runners(num_env_runners=args.num_runners, sample_timeout_s=300.0)
+        .training(
+            lr=tune.grid_search([0.0005, 0.0002, 0.0001]),
+            train_batch_size_per_learner=args.batch_size_per_learner,
+            num_epochs=args.number_of_epochs,
+        )
+    )
+
+    # Creating a tuner instance to manage the trials
+    tuner = tune.Tuner(
+        config.algo_class,
+        param_space=config,
+        # Specify a stopping criterion. Note that the criterion has to match one of the
+        # pretty printed result metrics from the results returned previously by
+        # ``.train()``
+        # TODO: Doesn't work properly so far. The stop condition is not read in.
+        run_config=train.RunConfig(
+            stop={"CD": 0.2},
+        ),
+    )
+
+    # Run the Tuner and capture the results.
+    tuner.fit()
+
+
+if __name__ == "__main__":
+    main()
