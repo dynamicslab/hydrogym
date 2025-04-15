fa7ef06e445e2ec58413ced7b4c2184cb5fc934e
sajeda <smokbel@uwaterloo.ca>
2025-04-15 23:19:19 +0000

    further changes

--- hydrogym/jax/utils/io.py
+++ hydrogym/jax/utils/io.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp 
+import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
 from matplotlib.animation import PillowWriter
@@ -14,7 +15,6 @@ class LogCallback(CallbackBase):
     def __init__(
         self,
         postprocess: Callable,
-        nvals, 
         interval: Optional[int] = 1,
         filename: Optional[str] = None,
         print_fmt: Optional[str] = None 
@@ -23,20 +23,10 @@ class LogCallback(CallbackBase):
         self.filename = filename
         self.postprocess = postprocess
         self.print_fmt = print_fmt
-        self.data = jnp.zeros((1, nvals + 1))
         
-    def __call__(self, iter: int, t: float, flow):
-        if super().__call__(iter, t, flow):
-            new_data = jnp.array([t, *self.postprocess(flow)], ndmin=2)
-            if iter == 0:
-                self.data[0, :] = new_data
-            else:
-                self.data = jnp.append(self.data, new_data, axis=0)
-
-            if self.filename is not None:
-                jnp.savetxt(self.filename, self.data)
-            if self.print_fmt is not None:
-                print(self.print_fmt.format(*new_data.ravel()))
+    def __call__(self, flow):
+        self.postprocess(flow)
+        np.savetxt(self.filename, self.postprocess(flow), fmt="%.5f") 
                 
       
 def create_animation(trajectory, gif_name, frame_interval_factor):
@@ -53,7 +43,7 @@ def create_animation(trajectory, gif_name, frame_interval_factor):
     simulation = jnp.fft.irfftn(trajectory, axes=(1,2))
 
     fig, ax = plt.subplots()
-    cax = ax.imshow(simulation[0], cmap='icefire',interpolation='nearest', vmin=-8, vmax=8)
+    cax = ax.imshow(simulation[0], cmap='viridis',interpolation='nearest', vmin=-8, vmax=8)
     fig.colorbar(cax)
     
     num_frames = len(simulation)
