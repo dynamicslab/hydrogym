b5711b91c667229e807671e9138b7f65f3c6cfcb
yuninw <yuninw@umich.edu>
2026-03-12 09:08:56 -0400

    Update yw_dev the repository via branch yw/hydrogym_v1.0_nekTest

--- github-update.sh
+++ github-update.sh
@@ -4,4 +4,4 @@ branch_name="yw/hydrogym_v1.0_nekTest"
 # Update the repository
 git add .
 git commit -m "Update $repo_name the repository via branch $branch_name"
-git push $repo_name $branch_name
+git push --force $repo_name $branch_name
