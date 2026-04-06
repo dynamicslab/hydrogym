d964bb23e2517f985fc44b97afbe4c5e005f4811
Ludger Paehler <paehlerludger@gmail.com>
2026-04-05 23:48:02 -0700

    Add logos to webpage (#241)
    * Edit title, and title description of webpage.
    
    * add temporary logos.

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -39,8 +39,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`HydroGym`}
+      description="A Reinforcement Learning Platform for Fluid Dynamics">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
