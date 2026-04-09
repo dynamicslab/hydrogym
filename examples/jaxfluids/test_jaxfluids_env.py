93be3a791e146e41ec8fefdcd9ad676c81deca1b
dbezgin <deniz.bezgin@tum.de>
2026-04-09 15:19:12 +0000

    adding example

---
+++ examples/jaxfluids/test_jaxfluids_env.py
@@ -0,0 +1,32 @@
+from hydrogym.jaxfluids import Nozzle2D
+
+
+def main():
+    env_config = {"environment_name": "Nozzle2D_coarse"}
+
+    env = Nozzle2D(env_config=env_config)
+
+    observation, info = env.reset(seed=0)
+    env.render()
+
+    for i in range(1000):
+
+        # Random action
+        # action = env.action_space.sample()       
+
+        # Fixed action
+        action = [1.0, 0.0]                  
+
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if env.env_step % 10 == 0:
+            env.render()
+
+        if terminated or truncated:
+            observation, info = env.reset()
+
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
